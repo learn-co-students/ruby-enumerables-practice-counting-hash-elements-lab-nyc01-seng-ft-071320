fe5f1f7172a9b983a060543d29394a3b082e1b32
amitdesh <amitdesh2014@gmail.com>
2020-07-13 21:00:11 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  counter = 0
+  while counter < array.length do
+    new_hash[array[counter]] = array.count(array[counter])
+    counter +=1
+  end
+  return new_hash
 end
  
